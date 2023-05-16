61891c62dbf64bdb1cd23df407305058a08ccbad
unknown <Dimples>
2023-05-16 15:24:20 +0800

    Added Axios Functionality to access OpenAI

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-const { app, BrowserWindow } = require('electron');
+//Imported Modules
+const { app, BrowserWindow, ipcMain} = require('electron');
 const path = require('path');
+const axios = require('axios');
 
+//Main Window
 const isDev = true;
 
 const createWindow = () => {
@@ -20,6 +23,9 @@ if ( isDev){
 };
 
 app.whenReady().then(() => {
+  //Initializa Functions
+  ipcMain.handle('axios.openAI', openAI)
+
   createWindow()
 
   app.on('activate', () => {
@@ -34,3 +40,33 @@ app.on('window-all-closed', () => {
     app.quit()
   }
 });
+
+//Main Functions
+  async function openAI(event, phrase){
+    let res = null;
+
+    await axios({
+      method: 'post',
+      url: 'https://api.openai.com/v1/completions',
+      data: {
+        model: "text-davinci-003",
+        prompt: "Create an analogy for this phrase:\n\n" + phrase,
+        temperature: 0.5,
+        max_tokens: 60,
+        top_p: 1.0,
+        frequency_penalty: 0.0,
+        presence_penalty: 0.0
+      },
+      headers: {
+        'Content-Type': 'application/json', 
+        'Authorization': 'Bearer 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'
+      }
+    }).then(function (response) {
+      res = response.data;
+    })
+    .catch(function (error) {
+      res = error;
+    });
+
+  return res;
+}
--- renderer/js/renderer.js
+++ renderer/js/renderer.js
@@ -1,2 +1,17 @@
-const information = document.getElementById('info')
-information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`
+/*const information = document.getElementById('info')
+information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`*/
+
+const form = document.getElementById("phrase");
+
+if(form){
+    form.onsubmit = async function (e) {
+        e.preventDefault(); 
+
+        const formData = new FormData(form);
+
+        //console.log(formData.get("phrase"));
+
+        const response = await window.axios.openAI(formData.get("phrase"));
+        document.getElementById("output").innerHTML = JSON.stringify(response.choices[0].text).replace(/\\n/g, '');
+    };
+}
