6a2864615338da1fdd683eac0851d985ecf455cd
unknown <Dimples>
2023-05-16 08:53:08 +0800

    Added Common Boilerplate

--- main.js
+++ main.js
@@ -1,13 +1,23 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+const isDev = true;
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600
-  })
+    width: isDev ? 1200 : 600,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
 
-  win.loadFile('./renderer/index.html')
+if ( isDev){
+    win.webContents.openDevTools();
 }
+
+  win.loadFile(path.join(__dirname, './renderer/index.html'));
+};
 
 app.whenReady().then(() => {
   createWindow()
@@ -16,11 +26,11 @@ app.whenReady().then(() => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow()
     }
-  })
-})
+  });
+});
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
   }
-})
+});
